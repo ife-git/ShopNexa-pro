038dc7773fb6b9618329f9b5ce054fb240762ca0
Ife.D <ifeoluwa.daramola@stu.cu.edu.ng>
2026-03-25 12:30:55 +0100

    V9

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // 🔥 IMPORTANT
   plugins: [react()],
   server: {
     proxy: {
